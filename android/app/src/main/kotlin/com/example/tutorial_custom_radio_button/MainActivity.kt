e1775b91be024d50d924226ca8e31ee612d195d9
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-03-17 22:59:32 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/tutorial_custom_radio_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tutorial_custom_radio_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'custom_radio_button_group.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: CustomRadioButtonGroup(),
+    );
+  }
+}
+
